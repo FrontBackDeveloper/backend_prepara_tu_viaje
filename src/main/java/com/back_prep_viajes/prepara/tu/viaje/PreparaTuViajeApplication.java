9b8531dbb49f8a676e4a5bee03f84fcfe11b60c0
GustavoBeltran <gustavodbeltran@gmail.com>
2022-08-22 19:04:00 -0300

    comienzo con el backend, creé los packages ppales

---
+++ src/main/java/com/back_prep_viajes/prepara/tu/viaje/PreparaTuViajeApplication.java
@@ -0,0 +1,13 @@
+package com.back_prep_viajes.prepara.tu.viaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparaTuViajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreparaTuViajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back_prep_viajes/prepara/tu/viaje/PreparaTuViajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back_prep_viajes.prepara.tu.viaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreparaTuViajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
